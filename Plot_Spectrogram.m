fe35443e1dc3f498b0d5d6470d97faddf5d02506
madtrack133 <madtrack133@gmail.com>
2025-05-07 17:14:42 +0800

    Added Spectograms

--- Plot_Spectrogram.m
+++ Plot_Spectrogram.m
@@ -2,7 +2,7 @@
 % Plot Spectrogram
 %
 
-[y,Fs]=audioread('NoisySignal.wav');
+[y,Fs]=audioread('NoiseRef2.wav');
 spectrogram(y,512,256,512,Fs,'yaxis');
+title('NoiseRef2 v2(n) spectrogram');
 colorbar;
-colormap gray;
