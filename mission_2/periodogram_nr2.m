749793021aedecfb7ef6012a6d0303c886be311f
Wesley Dalbock <dalbockwesley@gmail.com>
2025-05-21 15:17:52 +0800

    cleaned up periodogram file

--- mission_2/periodogram_nr2.m
+++ mission_2/periodogram_nr2.m
@@ -1,72 +1,39 @@
 [nr2, fs2] = audioread('../NoiseRef2.wav');
 
 N_window = length(nr2);
+my_nfft = N_window;
 
 % -------------------- Standard & Modified Periodograms --------------------
 
-% standard periodogram
-[px_rectangular, f_rectangular] = periodogram(nr2(1:N_window), rectwin(N_window), [], fs2);
+% Standard periodogram
+[px_rectangular, f_rectangular] = periodogram(nr2(1:N_window), rectwin(N_window), my_nfft, fs2);
 
-% modified periodograms
-[px_bartlett, f_bartlett] = periodogram(nr2(1:N_window), bartlett(N_window), [], fs2);
-[px_blackman, f_blackman] = periodogram(nr2(1:N_window), blackman(N_window), [], fs2);
-[px_hamming, f_hamming] = periodogram(nr2(1:N_window), hamming(N_window), [], fs2);
-[px_hann, f_hann] = periodogram(nr2(1:N_window), hann(N_window), [], fs2);
+% Modified periodograms
+[px_bartlett, f_bartlett] = periodogram(nr2(1:N_window), bartlett(N_window), my_nfft, fs2);
+[px_blackman, f_blackman] = periodogram(nr2(1:N_window), blackman(N_window), my_nfft, fs2);
+[px_hamming, f_hamming] = periodogram(nr2(1:N_window), hamming(N_window), my_nfft, fs2);
+[px_hann, f_hann] = periodogram(nr2(1:N_window), hann(N_window), my_nfft, fs2);
 
-% -------------------- Welch's method --------------------
+px_all = [px_rectangular px_bartlett px_blackman px_hamming px_hann];
+f_all = [f_rectangular f_bartlett f_blackman f_hamming f_hann];
 
-% overlap percentage
-overlap = 0.5;
-
-% length of window
-L = 512;
-
-window1 = rectwin(L);
-window2 = bartlett(L);
-window3 = blackman(L);
-window4 = hamming(L);
-window5 = hann(L);
-
-% overlap value
-noverlap = L * overlap;
-
-% default value for nfft: the number of FFT points used
-[px_rect_w, f_rect_w] = pwelch(nr2, window1, noverlap, [], fs2);
-[px_bart_w, f_bart_w] = pwelch(nr2, window2, noverlap, [], fs2);
-[px_black_w, f_black_w] = pwelch(nr2, window3, noverlap, [], fs2);
-[px_hamm_w, f_hamm_w] = pwelch(nr2, window4, noverlap, [], fs2);
-[px_hann_w, f_hann_w] = pwelch(nr2, window5, noverlap, [], fs2);
-
+names = {'Rectangular' 'Bartlett' 'Blackman' 'Hamming' 'Hanning'};
 
 % -------------------- Plots --------------------
+
 xlabel('Hz');
 ylabel('dB');
 
-plot(f_rectangular, 10*log10(px_rectangular), f_rect_w, 10*log10(px_rect_w));
-title('Rectangular');
-disp('-- Hit any key to continue --'), pause;
+for i=1:5
+    
+    % Index current values
+    px_current = px_all(:, i);
+    f_current = f_all(:, i);
+    name = names(i);
 
-plot(f_bartlett, 10*log10(px_bartlett), f_bart_w, 10*log10(px_bart_w));
-title('Bartlett');
-disp('-- Hit any key to continue --'), pause;
+    % Plot current values
+    plot(f_current, 10*log10(px_current));
+    title(name);
+    disp('-- Hit any key to continue --'), pause;
 
-plot(f_blackman, 10*log10(px_blackman), f_black_w, 10*log10(px_black_w));
-title('Blackman');
-disp('-- Hit any key to continue --'), pause;
-
-plot(f_hamming, 10*log10(px_hamming), f_hamm_w, 10*log10(px_hamm_w));
-title('Hamming');
-disp('-- Hit any key to continue --'), pause;
-
-plot(f_hann, 10*log10(px_hann), f_hann_w, 10*log10(px_hann_w));
-title('Hann');
-disp('-- Hit any key to continue --'), pause;
-
-
-plot(f_hann_w, 10*log10(px_hann_w), ...
-    f_hamm_w, 10*log10(px_hamm_w), ...
-    f_black_w, 10*log10(px_black_w), ...
-    f_bart_w, 10*log10(px_bart_w), ...
-    f_rect_w, 10*log10(px_rect_w))
-title('All Windows Using Welchs Method')
-legend('Hann','Hamming','Blackman','Bartlett', 'Rectangular');
+end
